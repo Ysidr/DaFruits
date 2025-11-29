da2384e8c0a6f64d472850c298df95a00d1168a0
Ysidro <39842830+Ysidr@users.noreply.github.com>
2025-11-29 21:22:51 +0100

    get input Text

--- AngularLogic/src/app/fruitlist/fruitlist.component.ts
+++ AngularLogic/src/app/fruitlist/fruitlist.component.ts
@@ -1,13 +1,23 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SingleFruitComponent } from './single-fruit/single-fruit.component';
+
 
 @Component({
   selector: 'app-fruitlist',
-  imports: [CommonModule],
+  imports: [CommonModule, SingleFruitComponent],
   templateUrl: './fruitlist.component.html',
   styleUrl: './fruitlist.component.scss'
 })
 export class FruitlistComponent {
+fontColorGood = 'lightgreen';
+fontColorBad = 'lightcoral';
+
+
+  nameLog(name: string) {
+    console.log(name);
+  }
+
   fruitlist = [
     {
       name: "Apfel",
---
+++ AngularLogic/src/app/fruitlist/single-fruit/single-fruit.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-single-fruit',
+  imports: [FormsModule],
+  templateUrl: './single-fruit.component.html',
+  styleUrl: './single-fruit.component.scss'
+})
+export class SingleFruitComponent {
+  @Input() fruit = {
+    name: "Apfel",
+    img: "apple.png",
+    description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
+    genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
+    stars: 2.3,
+    reviews: [{ name: "Kevin W.", text: "ist lecker" }, { name: "Arne P.", text: "nicht so meins" }],
+  }
+
+  inputData: string = '';
+
+  @Output() fruitname = new EventEmitter<string>();
+
+  sendInputData(){
+    this.fruitname.emit(this.inputData);
+  }
+
+}
