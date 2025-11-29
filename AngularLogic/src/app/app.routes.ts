444a8285cade4226b4311bddb1de69478d4a0538
Ysidro <39842830+Ysidr@users.noreply.github.com>
2025-11-29 19:05:17 +0100

    First  commit

---
+++ AngularLogic/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { FruitlistComponent } from './fruitlist/fruitlist.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: FruitlistComponent
+    }
+];
+
